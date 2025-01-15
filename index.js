684bb00c65d64abed85e43cc73f8455c9692623f
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2025-01-15 09:31:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 if (process.version !== 'v20.18.0') 
-    console.warn(`HEY!!! your on node ${process.version}, this app is built to run on v20.18.0. expect errors.`);
-
+    console.warn(`\n\x1b[31m\x1b[1mHEY!!! your on node \x1b[36m${process.version}\x1b[31m, this app is built to run on \x1b[32m\x1b[3mv20.18.0\x1b[31m. \x1b[4m\x1b[21mEXPECT ERRORS.\x1b[0m\n`);
 require('./progress-event');
 require('./fake-canvas');
 const { init, addThreeHelpers } = require('3d-core-raub');
