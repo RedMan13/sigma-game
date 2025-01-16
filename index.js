cd1ae5f547521cb5e20d9b40c7c5c846399d3941
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2025-01-16 09:26:15 -0800

    fixup for latest 3d-core

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // ansi escape codes my beloved
 if (!process.version.startsWith('v20')) 
     console.warn(`\n\x1b[31m\x1b[1mHEY!!! your on node \x1b[36m${process.version}\x1b[31m, this app is built to run on \x1b[32m\x1b[3mv20\x1b[31m. \x1b[4m\x1b[21mEXPECT ERRORS.\x1b[0m\n`);
-require('./progress-event');
-require('./fake-canvas');
 const { init, addThreeHelpers } = require('3d-core-raub');
 const { doc, gl, requestAnimationFrame } = init({ isGles3: true });
 addThreeHelpers(require('three'), gl);
 
+require('./progress-event');
+require('./fake-canvas');
 const tick = require('./game');
 const animate = time => {
     tick(time);
