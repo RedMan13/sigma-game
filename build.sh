41e4d214364eae3585ed29cec45b5c673f672caf
redman13 <godslayerakp@gmail.com>
2025-01-17 09:34:26 -0800

    setup build script

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 npm i
-pkg -C GZip ./index.js --out-path ./dist
-cp ./assets ./dist/assets -r
-cd ./dist
-mv index-linux lin-simga
-mv index-macos mac-simga
-mv index-win.exe win-simga.exe
-cd ..
-zip -r ./prod.zip ./dist
-rm -rf ./dist
+pkg -C GZip ./index.js
+mkdir ./lin
+mkdir ./mac
+mkdir ./win
+mv index-linux ./lin/simga
+mv index-macos ./mac/simga
+mv index-win.exe ./win/simga.exe
+cp ./assets ./lin/assets -r
+cp ./assets ./mac/assets -r
+cp ./assets ./win/assets -r
+zip -r ./simga-lin.zip ./lin
+zip -r ./simga-mac.zip ./mac
+zip -r ./simga-win.zip ./win
+rm -rf ./win
+rm -rf ./mac
+rm -rf ./lin
