d8f732e31af9ac653147394273af67f7ce5e8c3e
redman13 <godslayerakp@gmail.com>
2025-01-19 15:16:38 -0800

    begin adding complete lighting (pov its laggy as shit)

--- game/states.js
+++ game/states.js
@@ -1,18 +1,30 @@
-const { Vector3, Scene, PerspectiveCamera, AmbientLight, SpriteMaterial, Sprite, Texture, NearestFilter, NearestMipMapLinearFilter } = require('three');
+const { Vector3, Scene, PerspectiveCamera, AmbientLight, RectAreaLight, HemisphereLight, PointLight, SpriteMaterial, Sprite, Texture, NearestFilter, NearestMipMapLinearFilter } = require('three');
 const { GLTFLoader } = require('./cloned/GLTFLoader');
 
 const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
 camera.rotation.set(0,0,0, 'YXZ');
 
 const scene = new Scene();
-const ambientLight = new AmbientLight('white', 1);
-scene.add(ambientLight);
-
+const lightBar1 = new RectAreaLight('#cb9eff', 1, 16, 1);
+lightBar1.rotation.y = Math.PI;
+lightBar1.position.x = 1.5;
+lightBar1.position.z = -11.5;
+lightBar1.position.y = 1.5;
+const lightBar2 = new RectAreaLight('#cb9eff', 1, 16, 1);
+lightBar2.position.copy(lightBar1);
+lightBar2.position.z += 0.5;
+const pointLight = new PointLight('white', 1, 6, 0.10);
+pointLight.position.x = -3;
+pointLight.position.y = 0.5;
+pointLight.position.z = -1;
+scene.add(new AmbientLight('white', 1));
+// scene.add(lightBar1, lightBar2, pointLight);
+/*
 const loader = new GLTFLoader();
 loader.load('assets/bunker.gltf', bunkerData => {
     const bunker = bunkerData.scene;
     scene.add(bunker);
-});
+});*/
 
 const mat = new SpriteMaterial();
 const guiSprite = new Sprite(mat);
